06348d474fabf2712f7f58beadc1141bb2a605a2
WollyOT <smith.paul.1232@gmail.com>
2020-11-16 21:03:16 -0500

    finished auth flow

---
+++ services/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {  // puts identifying info into a cookie
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {  // takes identifying info out of a cookie
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'      
+  }, 
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }) //checks if user is new or existing
+      .then((existingUser) => {            // this only runs if an a user is found (new or existing)
+        if (existingUser) {                // if true we already have a record of them
+          done(null, existingUser);
+        } else {
+          new User ({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+        }
+      })    
+  })
+);
